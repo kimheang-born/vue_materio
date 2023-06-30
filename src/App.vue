37531420c8826ab09a3bd7978bd9d27d8524ca89
Kimheang <kimheang135@gmail.com>
2023-06-30 11:11:10 +0700

    chore(login): fetching user login

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <script setup>
+import { onBeforeMount } from 'vue'
+import { useStore } from 'vuex'
+
+const store = useStore()
+
+onBeforeMount(() => {
+  store.dispatch('tryLogin')
+})
 </script>
 
 <template>
   <VApp>
     <RouterView />
-    <!-- <UpgradeToPro /> -->
   </VApp>
 </template>
