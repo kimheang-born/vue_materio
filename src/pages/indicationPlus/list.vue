08e90e8f8a2608cf957f91e476d770e8c0630b9d
Kimheang <kimheang135@gmail.com>
2023-07-05 14:44:51 +0700

    chore: move table inside the list

--- src/pages/indicationPlus/list.vue
+++ src/pages/indicationPlus/list.vue
@@ -1,5 +1,37 @@
 <script setup>
-import DataTable from '@/views/pages/tables/indicationPlus/dataTable.vue'
+import { onBeforeMount, computed } from 'vue'
+import { useStore } from 'vuex'
+
+const store = useStore()
+
+const page = ref(1)
+const isLoading = ref(false)
+const error = ref(null)
+
+const mappedCases = computed(() => store.getters['indicationPlus/mappedCases'])
+
+const loadCases = async () => {
+  isLoading.value = true
+
+  try {
+    await store.dispatch('indicationPlus/fetchCases')
+  } catch (err) {
+    error.value = err.message || 'The data could not be retrieved.'
+  }
+
+  isLoading.value = false
+}
+
+const headers = [
+  'Cases Number',
+  'Status',
+  'Property Type',
+  'Created At',
+]
+
+onBeforeMount(() => {
+  loadCases()
+})
 
 const loading = ref(false)
 
@@ -84,6 +116,68 @@ const onClick = () => {
   </VCard>
 
   <VCard>
-    <DataTable />
+    <VTable
+      :headers="headers"
+      fixed-header
+      height="450px"
+    >
+      <thead>
+        <tr>
+          <th
+            v-for="header in headers"
+            id="cases"
+            :key="header"
+            class="text-left able-rounded"
+            hide-default-footer
+            disable-sort
+          >
+            {{ header }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="item in mappedCases"
+          :key="item.id"
+        >
+          <td
+            class="text-sm"
+            v-text="item.id"
+          />
+          <td>
+            <VChip
+              size="small"
+              :color="item.color"
+              class="text-capitalize"
+            >
+              {{ item.status }}
+            </VChip>
+          </td>
+          <td
+            class="text-sm"
+            v-text="item.propertyTypeDynamicName"
+          />
+          <td
+            class="text-sm"
+            v-text="$filters.timeAgo(item.createdAt)"
+          />
+        </tr>
+      </tbody>
+    </VTable>
+    <div class="text-center">
+      <VContainer>
+        <VRow justify="center">
+          <VCol cols="8">
+            <VContainer class="max-width">
+              <VPagination
+                v-model="page"
+                class="my-4"
+                :length="15"
+              />
+            </VContainer>
+          </VCol>
+        </VRow>
+      </VContainer>
+    </div>
   </VCard>
 </template>
